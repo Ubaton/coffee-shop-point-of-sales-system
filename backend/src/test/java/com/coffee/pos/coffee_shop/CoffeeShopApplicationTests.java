b7cd73026b464473647cf8f45287f2408f0b2869
Raymond <32289508+Ubaton@users.noreply.github.com>
2025-01-16 21:44:28 +0200

    first commit

---
+++ backend/src/test/java/com/coffee/pos/coffee_shop/CoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.pos.coffee_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
